85c6d81ddcb22632dc9a2ec2db89065068695386
ntaoo <ntaoo.g@gmail.com>
2013-12-24 18:39:02 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angularwithrails::Application.config.secret_key_base = '2b946f518a6f5b5810661ae3c6ff6d056562ae3bef67d51951de17686a6199c70a46594605d417cdc62f4d7234cebc15ed881f23552f127af71a0f838da1bc28'
